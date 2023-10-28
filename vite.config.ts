74e4b3b504a01f7b7f03c1d78526a3daa96014e2
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-10-28 14:25:30 +0300

    changed vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RA_Components_task-1/'
+  base: 'https://johnnystorm19.github.io/RA_Components_task-1/'
 })
